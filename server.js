dc6dffed1738f99480b17c89e5fbfab374eeca43
Cameron Woolstenhulme <cjwooly@gmail.com>
2019-02-13 19:26:34 -0500

    add correct headers for POST requests

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
     next();
 });
 var router = express.Router();
